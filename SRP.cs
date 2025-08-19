2a7f2e489e572ae958ac9fd4863ab3e5ce61bb14
Tsj Aslam <aslam.exe1@gmail.com>
2025-08-19 17:00:30 +0800

    add srp class

---
+++ SRP.cs
@@ -0,0 +1,10 @@
+public class EmployeeService
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void EmployeeRegistration(EmployeeService employee)
+        {
+            StaticData.Employees.Add(employee);
+        }
+    }
